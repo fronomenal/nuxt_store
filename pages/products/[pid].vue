01691280f10b0b68cce1e7f23b760e94f6c8473b
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-11-03 23:22:59 +0000

    Add navigation links to index page

--- pages/products/[pid].vue
+++ pages/products/[pid].vue
@@ -8,7 +8,6 @@ const {pid} = useRoute().params
   <div>
     <h2>Products Details: {{ pid }}</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate dolores ad, saepe tempore quod, aliquam ex unde quae quisquam, praesentium facilis quo delectus. Debitis laboriosam placeat, nihil non expedita cumque?</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate dolores ad, saepe tempore quod, aliquam ex unde quae quisquam, praesentium facilis quo delectus. Debitis laboriosam placeat, nihil non expedita cumque?</p>
   </div>
 </template>
 
