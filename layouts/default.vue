ac19b31ab906979f7e954f9aae36f4afde86894b
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-11-09 23:58:49 +0000

    Add tailwindcss

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,15 @@
 <template>
-  <header>
-    <nav> 
+  <header class="shadow-sm bg-white">
+    <nav class="container mx-auto p-4 flex justify-between"> 
       <NuxtLink to="/">Nuxt Merch</NuxtLink>
-      <ul>
+      <ul class="flex gap-4">
         <li><NuxtLink to="/">Home</NuxtLink></li>
         <li><NuxtLink to="/about">About</NuxtLink></li>
         <li><NuxtLink to="/products">Products</NuxtLink></li>
       </ul>
     </nav>
   </header>
-  <main>
+  <main class="container mx-auto p-4">
     <slot />
   </main>
 </template>
--- layouts/products.vue
+++ layouts/products.vue
@@ -1,14 +1,14 @@
 <template>
-  <header>
-    <nav> 
+  <header class="shadow-sm bg-white">
+    <nav class="container mx-auto p-4"> 
       <NuxtLink to="/products">Nuxt Merch: Products</NuxtLink>
     </nav>
   </header>
-  <main>
+  <main class="container mx-auto p-4">
     <slot />
   </main>
-  <footer>
-    <ul>
+  <footer class="container mx-auto p-4 flex justify-between border-t-2">
+    <ul class="flex gap-4">
       <li><NuxtLink to="/">Home</NuxtLink></li>
       <li><NuxtLink to="/about">About</NuxtLink></li>
       <li><NuxtLink to="/products">Products</NuxtLink></li>
