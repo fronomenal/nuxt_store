bd9cabed4e620063f68ec0284f267d9f10f1b29e
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-11-10 00:14:27 +0000

    Create tailwind button component

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
       <ul class="flex gap-4">
         <li><NuxtLink to="/">Home</NuxtLink></li>
         <li><NuxtLink to="/about">About</NuxtLink></li>
-        <li><NuxtLink to="/products">Products</NuxtLink></li>
+        <li><NuxtLink to="/products" class="btn">Products</NuxtLink></li>
       </ul>
     </nav>
   </header>
