db738157d9983bb3038b756c81bd341edaa69182
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-11-21 23:55:33 +0000

    setup for payment processing

---
+++ pages/failure.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <h1>Failed to complete stripe payment</h1>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/success.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <h1>Thank you for the purchase</h1>
+  </div>
+</template>
+
+<style scoped></style>
