7668b5a55e6c3fc0c8a34e5c038329df81bc7ebb
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-11-22 23:57:27 +0000

    implement all cartitem store methods

--- stores/cartItems.ts
+++ stores/cartItems.ts
@@ -11,16 +11,51 @@ export type CartItem = {
 export const useCartStore = defineStore("cart-local-store", {
   state: ()=> ({cartItems: useLocalStorage<CartItem[]>("local-cart", [])}),
   getters:{
+    _get(id){
+      return this.cartItems.find( (item) => item.id == id );
+    },
     getItems(){
       return this.cartItems
     },
     getCount(){
       return this.cartItems.length;
+    },
+    getItemCount(id){
+      const count = this._get(id)?.quantity;
+
+      if(count) return count.quantity;
+
+      return 0;
     }
   },
   actions:{
     setItem(item: CartItem){
+      const itemRef = this._get(item.id);
+
+      if (itemRef){
+        itemRef.quantity++;
+        return;
+      }
+
       this.cartItems.push(item)
-    }
+    },
+    triggerItem(id: number){
+      const itemRef = this._get(id);
+
+      if(itemRef) !itemRef.checked
+
+    },
+    addItem(id: number){// increase existing item quantity
+      const itemRef = this._get(id);
+
+      if(itemRef) itemRef.quantity++;
+
+    },
+    subItem(id: number){// decrease existing item quantity
+      const itemRef = this._get(id);
+
+      if(itemRef) itemRef.quantity--;
+
+    },
   }
 });
