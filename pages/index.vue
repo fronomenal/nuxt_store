237f2bfcc7e41b27b3536d09aa439123001f9d40
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-12-12 02:41:25 +0000

    modify text on index page

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
 <template>
   <div>
     <h2>Home</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate dolores ad, saepe tempore quod, aliquam ex unde quae quisquam, praesentium facilis quo delectus. Debitis laboriosam placeat, nihil non expedita cumque?</p>
+    <p class="mb-3 text-xl">Welcome to Nuxt Store: Please Navigate to the products page to  view our catalog</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate dolores ad, saepe tempore quod, aliquam ex unde quae quisquam, praesentium facilis quo delectus. Debitis laboriosam placeat, nihil non expedita cumque?</p>
   </div>
 </template>
